e1a83a0338b5301c1adfabe3b372f808c5d1c800
Roger Russel <roger.neris@locaweb.com.br>
2016-07-07 20:49:33 -0300

    created gulp docker

--- gulp.sh
+++ gulp.sh
@@ -4,18 +4,25 @@ DIRINICIAL=$(pwd);
 DIRROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 APP=app
 DIRAPP=$DIRROOT/$APP
+GULP=mobilebudget-gulp
 
 function install {
- docker build -t $APP -f $DIRROOT/docker/gulp/Dockerfile . 
+ docker build -t $GULP -f $DIRROOT/docker/gulp/Dockerfile . 
+ docker run --name="$GULP" -v $DIRROOT:/data -d $GULP tail -f /dev/null
+ docker exec -it $GULP bash -c "npm install"
+}
+
+function run {
+  echo "" #docker run --name="$GULP" -v $DIRROOT:/data -d $GULP tail -f /dev/null
+
 }
 
 case "$1" in
-
   install)
     install
     ;;
-  about)
-    echo "https://hub.docker.com/r/webratio/phonegap/"
+  run)
+    run
     ;;
   *)
   echo "command $1 not found";
---
+++ gulp/sass.js
@@ -0,0 +1,16 @@
+module.exports = function (gulp, $) {
+  'use strict';
+
+  var gulp = require('gulp');
+  var shell = require('gulp-shell');
+
+  var nodeCommand = 'node-sass --output-style="compressed"';
+
+  var stylesheetsPrivatePath = 'resources/assets/sass';
+  var stylesheetsPublicPath = 'app/www/assets/css';
+
+  gulp.task('sass', shell.task([
+    nodeCommand + ' ' + stylesheetsPrivatePath + '/app.sass ' + stylesheetsPublicPath + '/app.css'
+  ]));
+
+};
